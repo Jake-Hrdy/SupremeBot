a37afb9a040568ff6f50a5e8677ed130738a4803
Hardy Man <jkhardy12@gmail.com>
2020-03-03 16:42:34 -0500

    Initialize browser and navigate to item on site

--- main.py
+++ main.py
@@ -10,24 +10,32 @@ class SupremeBot:
         self.info = info
 
     def init_browser(self):
-        self.browser = Browser()    # fix for chrome eventually?
+        self.browser = Browser("chrome")
 
     def find_product(self):
-        req = requests.get("{}{}{}".format(self.base, self.shop_ext, self.info["category"])).text   # sends back html of page
-        soup = bs4.BeautifulSoup(req, "lxml")   # allows us to work with html better
+        try:
+            req = requests.get("{}{}{}".format(self.base, self.shop_ext, self.info["category"])).text   # sends back html of page
+            soup = bs4.BeautifulSoup(req, "lxml")   # allows us to work with html better
+            
+            temp_link = []      # list of links that either had product name or color (should only be one element after loop)
+            for link in soup.find_all("a", href=True):
+                if link.text == self.info["product"] or link.text == self.info["color"]:
+                    temp_link.append(link["href"])
 
-        temp_link = []      # list of links that either had product name or color (should only be one element after loop)
-        for link in soup.find_all("a", href=True):
-            if link.text == self.info["product"] or link.text == self.info["color"]:
-                temp_link.append(link["href"])
+            self.final_link = temp_link[0]
+            print(self.final_link)
+            return True
 
-        self.final_link = temp_link[0]
-        print(self.final_link)
+        except:
+            False
+
+    def visit_site(self):
+        self.browser.visit("{}{}".format(self.base, self.final_link))
 
 
 if __name__ == "__main__":
+    # set product info
     info = {
-        # "driver": "geckodriver",
         "product": "Miles Davis Hooded Sweatshirt",
         "color": "Blue",
         "size": "Large",
@@ -46,5 +54,26 @@ if __name__ == "__main__":
         "ccv": "123"
     }
 
-    bot = SupremeBot(**info)
-    bot.find_product()
+    bot = SupremeBot(**info)    # initialize bot
+    # ================================================================================
+    ''' This will allow you to start the bot right before a drop, so that it will find
+        the product as soon as it's available '''
+    found_product = False
+    max_iter = 100
+    counter = 1
+    while not found_product and counter <= max_iter:
+        found_product = bot.find_product()
+        if counter == 1:
+            print("Tried ", counter, " time to find product")
+        else:
+            print("Tried ", counter, " times to find product")
+        counter += 1
+
+    if not found_product:
+        # product was never found in the while loop
+        raise Exception("Product couldn't be found.")
+    # ================================================================================
+
+    # continue because we found the product
+    bot.init_browser()
+    bot.visit_site()
